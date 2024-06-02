78d3481ab6f0767cc1dc97df3b46c063ba94f3c7
Andrew Chelimo <62815445+DaChelimo@users.noreply.github.com>
2024-06-03 00:09:11 +0300

    Initial commit

--- app/src/main/java/com/da_chelimo/generate/core/data/repo/local/dao/AppDatabase.kt
+++ app/src/main/java/com/da_chelimo/generate/core/data/repo/local/dao/AppDatabase.kt
@@ -2,8 +2,9 @@ package com.da_chelimo.generate.core.data.repo.local.dao
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.da_chelimo.generate.core.data.repo.local.entity.LocalEntity
 
-@Database(entities = [], version = 1, exportSchema = true)
+@Database(entities = [LocalEntity::class], version = 1, exportSchema = true)
 abstract class AppDatabase: RoomDatabase() {
 
     // DAOs
